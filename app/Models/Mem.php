3abe11b6bdf373bc60193c0bf0f8f3b8b98c4af1
Peoloper <Peoloperteen@example.com>
2022-01-31 12:12:11 +0100

    models

---
+++ app/Models/Mem.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
+
+class Mem extends Model
+{
+    use HasFactory;
+    protected $table = 'memes';
+
+    protected $fillable = [
+        'title',
+        'content',
+        'user_id',
+        'category_id',
+        'is_published',
+        'slug'
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function tags(): BelongsToMany
+    {
+        return $this->belongsToMany(Tag::class,'post_tags');
+    }
+
+}
