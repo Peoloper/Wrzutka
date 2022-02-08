23db00c30a1ca77487b5d43e3dbb6c0811f3df63
Peoloper <Peoloperteen@example.com>
2022-02-09 00:36:00 +0100

    Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,8 +14,9 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            UserTableSeeder::class,
-            RolesAndPermissionsTableSeeder::class,
+           // UserTableSeeder::class,
+          //  RolesAndPermissionsTableSeeder::class,
+            PhotosTableSeeder::class
         ]);
     }
 }
