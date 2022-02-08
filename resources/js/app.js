9e44792bc10291bcc6351f0e9c4ab2ce920bb412
Peoloper <Peoloperteen@example.com>
2022-02-09 00:18:23 +0100

    updated

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,16 +1,25 @@
+import SettingsComponent from "./components/SettingsComponent";
+
 require('./bootstrap')
 
 import { createApp } from 'vue'
 
-import NewMem from './components/NewMem'
+import NewMemComponent from './components/NewMemComponent'
+import getCommentsComponent from './components/GetCommentsComponent'
 import test from './components/test'
+import favoriteComponent from './components/FavoriteComponent'
+import LikeComponent from "./components/LikeComponent";
 
 
 
 const app = createApp({})
 
-app.component('new-mem', NewMem)
+app.component('new-mem', NewMemComponent)
+app.component('get-comments', getCommentsComponent)
 app.component('test', test)
+app.component('favorite', favoriteComponent)
+app.component('like', LikeComponent)
+app.component('setting', SettingsComponent)
 
 
 app.mount('#app')
