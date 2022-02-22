48a48ddb212b705ff15a6730478e4472f7dec7cf
Peoloper <Peoloperteen@example.com>
2022-02-22 12:38:16 +0100

    partials_blade

--- resources/views/partials/AuthUser.blade.php
+++ resources/views/partials/AuthUser.blade.php
@@ -10,23 +10,17 @@
         </div>
         <div class="card-body">
             <div class="group-card text-center">
-                <a href="{{route('profile.index', ['user' => auth()->user()->name])}}"><button class="button btn-primary">Profil</button></a>
+                <a href="{{route('profile.index', ['userName' => auth()->user()->name])}}"><button class="button btn-primary">Profil</button></a>
                 <a href="{{route('setting')}}"><button>Ustawienia</button></a>
                 <a href="{{route('favorite')}}"> <button>Ulubione</button></a>
-            </div>
-            <div class="group-card mt-3">
-                <p>Polecane Tagi:</p>
-                @foreach($tags as $tag)
-                    <span>#{{$tag->slug}}</span>
-                @endforeach
             </div>
         </div>
         <div class="row">
             <p>Polecamy</p>
-            <div class="col-md-8 col-lg-8 spr">
+            <div class="col-md-8 col-lg-8 marginAuto ">
                 <div class="group-card mt-3">
                     @foreach($featuredMemes as $mem)
-                        <div class=" spr mt-3" id="test">
+                        <div class="mt-3 border p-2">
                             <div class="group-card">
                                 <a href="{{route('mem.show', ['mem' => $mem, 'slug' => $mem->slug])}}">
                                     <img src="{{$mem->photos->path ?? null}}" alt="" class="card-img">
--- resources/views/partials/noAuthUser.blade.php
+++ resources/views/partials/noAuthUser.blade.php
@@ -14,10 +14,10 @@
         </div>
         <div class="row">
             <p>Polecamy</p>
-            <div class="col-md-8 col-lg-8 spr">
+            <div class="col-md-8 col-lg-8 marginAuto">
                 <div class="group-card mt-3">
                     @foreach($featuredMemes as $mem)
-                        <div class=" spr mt-3" id="test">
+                        <div class=" mt-3 border p-2">
                             <div class="group-card">
                                 <a href="{{route('mem.show', ['mem' => $mem, 'slug' => $mem->slug])}}">
                                     <img src="{{$mem->photos->path ?? null}}" alt="" class="card-img">
