915efdcec370ee16bc1e8c2b9f38deea9dde11dc
Peoloper <Peoloperteen@example.com>
2022-02-25 11:37:53 +0100

    updated Provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -32,18 +32,6 @@ class AppServiceProvider extends ServiceProvider
     {
         Paginator::useBootstrap();
 
-        Validator::extend('image64', function ($attribute, $value, $parameters, $validator) {
-            $type = explode('/', explode(':', substr($value, 0, strpos($value, ';')))[1])[1];
-            if (in_array($type, $parameters)) {
-                return true;
-            }
-            return false;
-        });
-
-        Validator::replacer('image64', function($message, $attribute, $rule, $parameters) {
-            return str_replace(':values',join(",",$parameters),$message);
-        });
-
         View::share('featuredMemes',  cache()->remember('featuredMemes', today()->endOfDay(), function()
         {
             return Mem::with(['photos', 'user.photos' ])
