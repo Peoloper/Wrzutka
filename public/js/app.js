67e2008d18f00280f246839bf74c126413ec0193
Peoloper <Peoloperteen@example.com>
2022-03-15 21:00:26 +0100

    updated app.js

--- public/js/app.js
+++ public/js/app.js
@@ -23470,12 +23470,12 @@ function render(_ctx, _cache, $props, $setup, $data, $options) {
   ))], 2
   /* CLASS */
   ), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_28, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("button", {
-    "class": "btn btn-success btn-block",
-    onClick: _cache[4] || (_cache[4] = function ($event) {
-      $options.addMem;
+    "class": "btn btn-success m-2",
+    onClick: _cache[4] || (_cache[4] = function () {
+      return $options.addMem && $options.addMem.apply($options, arguments);
     })
   }, "Dodaj mema"), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("button", {
-    type: "button",
+    "class": "btn btn-danger",
     onClick: _cache[5] || (_cache[5] = function () {
       return $options.reset && $options.reset.apply($options, arguments);
     })
@@ -28820,7 +28820,7 @@ __webpack_require__.r(__webpack_exports__);
 
 var ___CSS_LOADER_EXPORT___ = _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_0___default()(function(i){return i[1]});
 // Module
-___CSS_LOADER_EXPORT___.push([module.id, "\n.marginBott\n{\n    margin-bottom: 10px;\n}\n", ""]);
+___CSS_LOADER_EXPORT___.push([module.id, "\n.marginBott\r\n{\r\n    margin-bottom: 10px;\n}\r\n", ""]);
 // Exports
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (___CSS_LOADER_EXPORT___);
 
